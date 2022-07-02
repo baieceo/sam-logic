8d61352fe571117cf430b483224c119f74b5b0f4
chenxi5 <chenxi5@01zhuanche.com>
2022-07-02 16:03:19 +0800

    fix: 修复无自定义代码配置报错问题

--- packages/compile-code/src/addPlugins.js
+++ packages/compile-code/src/addPlugins.js
@@ -2,7 +2,7 @@ const INSERT_IMPORT_PLUGINS_COMMENT = '// import plugins here';
 const INSERT_USE_PLUGINS_COMMENT = '// use plugins here';
 const INSERT_USE_CUSTOM_CODE = '// use custom code';
 
-const addPlugins = (originalCode = '', plugins = [], options) => {
+const addPlugins = (originalCode = '', plugins = [], options = {}) => {
     const modifiedContent = originalCode
         .replace(new RegExp(INSERT_IMPORT_PLUGINS_COMMENT), () => {
             return plugins
@@ -13,7 +13,7 @@ const addPlugins = (originalCode = '', plugins = [], options) => {
             return plugins.map((_, index) => `logic.use(plugin${index});`).join('\n');
         })
         .replace(new RegExp(INSERT_USE_CUSTOM_CODE), () => {
-            return options.customCode['index.js'];
+            return options.customCode['index.js'] || '';
         });
     return modifiedContent;
 };
